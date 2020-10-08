84e40c3606039830c91e264bdce8deb770e51e55
asim <measim8@gmail.com>
2020-10-09 00:24:56 +0530

    comments-7

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,6 @@
-HOST           = "localhost"
-USER           = "root"
-PASSWORD       = "root"
+HOST           = "52.66.211.206"
+USER           = "myuser"
+PASSWORD       = "root@1234"
 DATABASE       = "movie_arena"
 SECRET_KEY     = "bruce wayne is the mask"
 
