3ce1bbf7980b045d5c66cb79c2f45c4d0111840e
asim <measim8@gmail.com>
2020-10-09 14:05:08 +0530

    comments-11

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,17 @@
-HOST           = "127.0.0.1"
-USER           = "myuser"
-PASSWORD       = "root@1234"
-DATABASE       = "movie_arena"
-SECRET_KEY     = "bruce wayne is the mask"
+import os
+current_dir = os.getcwd()
+if 'getmecab' in current_dir:
+
+    HOST           = "127.0.0.1"
+    USER           = "root"
+    PASSWORD       = "root"
+    DATABASE       = "movie_arena"
+    SECRET_KEY     = "bruce wayne is the mask"
+elif 'ubuntu' in current_dir:
+    HOST           = "127.0.0.1"
+    USER           = "myuser"
+    PASSWORD       = "root@1234"
+    DATABASE       = "movie_arena"
+    SECRET_KEY     = "bruce wayne is the mask"
 
 
